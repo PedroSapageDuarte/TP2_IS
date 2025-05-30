544f593ad9e61cea8dbd75f9ebf641245cdf03ce
PedroSapageDuarte <al2024116514@alunos.utad.pt>
2025-05-30 14:18:57 +0100

    REST API
    Código para comunicação com a base de dados (com GET e POST) através de uma REST API

---
+++ TP1_Prod/Pecas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TP1_Prod
+{
+    public class Pecas
+    {
+        public int ID_Produto { get; set; }
+        public string CodigoPeca { get; set; }
+        public DateTime DataProducao { get; set; }
+        public TimeSpan HoraProducao { get; set; }
+        public int TempoProducao { get; set; }
+        public string CodigoResultado { get; set; }
+        public DateTime DataTeste { get; set; }
+        
+        //NOVO
+        //Se quisermos expor custos:
+        public decimal? CustoProducao { get; set; }
+        public decimal? Lucro { get; set; }
+        public decimal? Prejuizo { get; set; }
+    }
+}
