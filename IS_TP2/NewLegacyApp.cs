a9349b3f954ae218b496bfd261c57a8f3d14b97a
PedroSapageDuarte <al2024116514@alunos.utad.pt>
2025-05-30 17:51:45 +0100

    Ficheiros gerais
    Inclui a nova aplicação legada, bem como o receiver dos dados que são publicados da stream do rabbitmq, e a dashboard de data analytics

---
+++ IS_TP2/NewLegacyApp.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace IS_TP2
+{
+    //Nova aplicação Legada, bastante semelhante à anterior
+    public class NewLegacyApp
+    {
+        public string Codigo_Peca { get; set; }
+        public DateTime Data_Producao { get; set; }
+        public TimeSpan Hora_Producao { get; set; }
+        public int Tempo_Producao { get; set; }
+        public string Codigo_Resultado { get; set; }
+
+        private static readonly Random random = new Random();
+
+        public static NewLegacyApp GerarProdutoAleatorio()
+        {
+            string[] tiposProduto = { "aa", "ab", "ba", "bb" };
+            string tipo = tiposProduto[random.Next(tiposProduto.Length)];
+            string identificador = Guid.NewGuid().ToString("N").Substring(0, 6);
+            string codigoPeca = tipo + identificador;
+
+            DateTime dataProducao = DateTime.Now.Date;
+            TimeSpan horaProducao = DateTime.Now.TimeOfDay;
+            int tempoProducao = random.Next(10, 51);
+
+            string codigoResultado = GerarCodigoResultado();
+
+            return new NewLegacyApp
+            {
+                Codigo_Peca = codigoPeca,
+                Data_Producao = dataProducao,
+                Hora_Producao = horaProducao,
+                Tempo_Producao = tempoProducao,
+                Codigo_Resultado = codigoResultado
+            };
+        }
+
+        //Método para gerar resultado do teste
+        private static string GerarCodigoResultado()
+        {
+            int odd = random.Next(100); //Número entre 0 e 99
+
+            if (odd < 50)
+                return "01"; //50%
+            else if (odd < 65)
+                return "02"; //15%
+            else if (odd < 75)
+                return "03"; // 10%
+            else if (odd < 85)
+                return "04"; // 10%
+            else if (odd < 95)
+                return "05"; // 10%
+            else
+                return "06"; // 5%
+        }
+    }
+}
---
+++ IS_TP2/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Text.Json;
+using IS_TP2;
+using System.Net.Http;
+
+class Program
+{
+    static async Task Main()
+    {
+        //Configuração da Stream - cria o sistema e garante que a stream "prod" existe
+        var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
+        await streamSystem.CreateStream(new StreamSpec("prod") { MaxLengthBytes = 5_000_000_000 });
+        //Cria o producer para enviar mensagens à stream "prod"
+        var producer = await Producer.Create(new ProducerConfig(streamSystem, "prod"));
+
+        //Configuração AMQP - prepara a conexão e o canal com RabbitMQ
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        using var conn = await factory.CreateConnectionAsync();
+        using var channel = await conn.CreateChannelAsync();
+        await channel.ExchangeDeclareAsync("producao_exchange", ExchangeType.Topic);
+
+        //Gera dados a cada 7 segundos
+        while (true)
+        {
+            var produto = IS_TP2.NewLegacyApp.GerarProdutoAleatorio();
+            var mensagem = new
+            {
+                data = produto.Data_Producao.ToString("yyyy-MM-dd"),
+                hora = produto.Hora_Producao.ToString(@"hh\:mm\:ss"),
+                codigo_peca = produto.Codigo_Peca,
+                tempo_producao = produto.Tempo_Producao,
+                resultado_teste = produto.Codigo_Resultado
+            };
+
+            string json = JsonSerializer.Serialize(mensagem);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            //AMQP
+            var routingKey = mensagem.resultado_teste == "01"
+                ? "dados.producao.ok"
+                : "dados.producao.falha";
+            await channel.BasicPublishAsync("producao_exchange", routingKey, body);
+            Console.WriteLine($"Enviado: {json}");
+
+            //Envia para as streams
+            await producer.Send(new Message(body));
+            Console.WriteLine($"[STREAM] Publicado: {mensagem.codigo_peca}");
+
+            await Task.Delay(7000);
+        }
+    }
+}
